ccc78212ead00fb9df9186d16fcf0dd58ece5cda
Bryan Olivo <bryanolivo@Bryans-MacBook-Air-4.local>
2024-08-20 19:34:29 -0400

    code refactor, added react routing and navbar template

---
+++ src/AppRouter.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Navigation from './components/Navigation/Navigation';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path ='/nav' element = {<Navigation/>}/>
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navigation(){
+    return (
+        <header className="navigation">
+            <nav>
+                <div className="about">
+                    About
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navigation;
